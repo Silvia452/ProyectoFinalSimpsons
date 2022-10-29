4184709aae618093f1a93aac4f9bab1589813726
PabloFP64 <PabloFP64@gmail.com>
2022-10-29 19:11:03 +0200

    Creado inicio clase Escenario

---
+++ src/main/java/com/example/thesimpsons/model/Escenario.java
@@ -0,0 +1,27 @@
+package com.example.thesimpsons.model;
+
+public class Escenario {
+    private long id;
+    private String nombre;
+
+    public Escenario(long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
